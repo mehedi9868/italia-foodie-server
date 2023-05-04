2838d8b7b111c8e5e44757cd0adb22525bf75dd9
mehedi9868 <mehedi9868@gmail.com>
2023-05-04 11:58:47 +0600

    testimonial data not found fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const allData = require('./data/allData.json');
-const testimonials = require('./data/testimonials.json')
+const testimonials = require('./data/testimonials.json');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,6 +11,9 @@ app.get('/', (req, res) => {
     res.send(allData);
 });
 
+app.get('/testimonials', (req, res) => {
+    res.send(testimonials);
+});
 app.get('/:id', (req, res) => {
     const id = req.params.id;
     const data = allData.find(item => item.id === parseInt(id));
@@ -20,9 +23,7 @@ app.get('/:id', (req, res) => {
         res.status(404).send('No Data Found');
     }
 });
-app.get('/testimonials', (req, res) => {
-    res.send(testimonials);
-});
+
 
 app.listen(port, () => {
     console.log(`server is running on port: ${port}`);
